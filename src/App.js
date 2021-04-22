933679502620064ec087cc380d903c58faa08a9e
StarBurst <panda.code.lab@gmail.com>
2021-04-22 12:15:47 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import TodoList from "./Todo/TodoList";
+
+function App() {
+  return (
+    <div className="wrapper">
+      <h1>React Tutorial</h1>
+      <TodoList />
+    </div>
+  );
+}
+
+export default App;
